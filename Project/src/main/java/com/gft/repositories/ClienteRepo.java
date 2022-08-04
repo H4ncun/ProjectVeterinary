6480c86ceb0865509ed8195bb70ae6bff04c740b
LECL <lecl@gft.com>
2022-08-04 15:38:31 -0300

    "Commit Service e Repository with Client"

---
+++ Project/src/main/java/com/gft/repositories/ClienteRepo.java
@@ -0,0 +1,9 @@
+package com.gft.repositories;
+
+import com.gft.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepo extends JpaRepository<Cliente, Long> {
+}
--- Project/src/main/java/com/gft/service/ClienteService.java
+++ Project/src/main/java/com/gft/service/ClienteService.java
@@ -1,6 +1,20 @@
 package com.gft.service;
 
+import com.gft.entities.Cliente;
+import com.gft.repositories.ClienteRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClienteService {
 
+    final ClienteRepo clienteRepo;
+
+    public ClienteService(ClienteRepo clienteRepo) {
+        this.clienteRepo = clienteRepo;
+    }
+
+    public void salvar(Cliente cliente){
+        clienteRepo.save(cliente);
+    }
 }
 
