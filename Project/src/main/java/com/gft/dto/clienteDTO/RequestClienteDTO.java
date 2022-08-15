cda6fd0eb34e24eeb6662b1ad91b1d5f19c85fda
LECL <lecl@gft.com>
2022-08-15 16:54:51 -0300

    Refactoring

--- Project/src/main/java/com/gft/dto/clienteDTO/RequestClienteDTO.java
+++ Project/src/main/java/com/gft/dto/clienteDTO/RequestClienteDTO.java
@@ -1,16 +1,15 @@
 package com.gft.dto.clienteDTO;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import com.gft.dto.enderecoDTO.RequestEnderecoDTO;
-
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,20 +17,15 @@ import lombok.Setter;
 @Setter
 public class RequestClienteDTO {
 
-	@ApiModelProperty(value = "Nome do cliente", name = "nome", dataType = "String", example = "Marcos")
-	@NotBlank
-	@Size(max = 250)
+    @ApiModelProperty(value = "Nome do cliente", name = "nome", dataType = "String", example = "Marcos")
+    @NotBlank
+    @Size(max = 250)
     private String nome;
 
     private RequestEnderecoDTO endereco;
 
-<<<<<<< HEAD
-	@ApiModelProperty(value = "telefone do cliente", name = "telefone", dataType = "String", example = "11936482459")
+    @ApiModelProperty(value = "telefone do cliente", name = "telefone", dataType = "String", example = "11936482459")
     private String telefone;
-=======
-	@ApiModelProperty(value = "Telefone do cliente", name = "telefone", dataType = "int", example = "936482459")
-	@NotNull
-    private int telefone;
->>>>>>> a097b8d5505f7bcc0bd5731b7d91c7e30ec42646
+
 
 }
