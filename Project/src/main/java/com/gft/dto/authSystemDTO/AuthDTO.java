00fcfda37149eb6dd8a41d1cb23bd66bfdab525c
Thiago Frederico <thiagofrederico3@gmail.com>
2022-08-23 02:57:12 -0300

    Criando container da aplicação

--- Project/src/main/java/com/gft/dto/authSystemDTO/AuthDTO.java
+++ Project/src/main/java/com/gft/dto/authSystemDTO/AuthDTO.java
@@ -1,5 +1,8 @@
 package com.gft.dto.authSystemDTO;
 
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class AuthDTO {
 
+	@ApiModelProperty(value = "E-mail para o login", name = "email", dataType = "String", example = "cliente@gft.com")
+	@NotBlank
     private String email;
+	
+	@ApiModelProperty(value = "Senha para o login", name = "senha", dataType = "String", example = "123")
+	@NotBlank
     private String password;
 
 }
